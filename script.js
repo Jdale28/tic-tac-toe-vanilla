ec152b609334cc6b8955cb3e012ea037c52b36fe
Jordan Dale <jordan.dale2@gmail.com>
2019-02-01 17:57:31 -0500

    Can determine winner

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ const winCombos = [
 ];
 
 const cells = document.querySelectorAll(".cell");
+
+startGame();
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  createBoard = Array.from(Array(9).keys());
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
+}
+
+function turnClick(square) {
+  turn(square.target.id, humanPlayer);
+}
+
+function turn(squareId, player) {
+  createBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(createBoard, player);
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == humanPlayer ? "blue" : "red";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
+}
+
+// Basic AI
